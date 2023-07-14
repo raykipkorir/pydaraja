3533d9f9d5f84139704ce2b719e167b67f8e6be7
Raymond Kipkorir <raykipkorir02@gmail.com>
2023-07-14 18:21:37 +0300

    feat: add transaction-status, add account balance

--- pydaraja/payment.py
+++ pydaraja/payment.py
@@ -1,12 +1,18 @@
 """This module contains daraja API services"""
 
-__all__ = ["trigger_stk_push", "query_stk_push"]
+__all__ = [
+    "trigger_stk_push",
+    "query_stk_push",
+    "transaction_status",
+    "account_balance",
+]
 
 from datetime import datetime
 
 import requests
 from requests import Response
 
+from pydaraja import config
 from pydaraja.utils import (
     _format_phone_number,
     _generate_access_token,
@@ -14,14 +20,6 @@ from pydaraja.utils import (
     _validate_amount,
     _validate_phone_number,
 )
-
-BUSINESS_SHORTCODE: str = ""
-PASSKEY: str = ""
-CONSUMER_SECRET: str = ""
-CONSUMER_KEY: str = ""
-CALLBACK_URL: str = ""
-ACCOUNT_REFERENCE: str = "Pydaraja"
-TRANSACTION_DESC: str = "Python wrapper for mpesa api"
 
 
 def trigger_stk_push(phone_number: str, amount: int) -> Response:
@@ -35,17 +33,17 @@ def trigger_stk_push(phone_number: str, amount: int) -> Response:
     _validate_amount(amount)
     stk_push_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
     payload = {
-        "BusinessShortCode": BUSINESS_SHORTCODE,
+        "BusinessShortCode": config.BUSINESS_SHORTCODE,
         "Password": _generate_password(),
         "Timestamp": datetime.now().strftime("%Y%m%d%H%M%S"),
         "TransactionType": "CustomerPayBillOnline",
         "Amount": amount,
         "PartyA": phone_number,
-        "PartyB": BUSINESS_SHORTCODE,
+        "PartyB": config.BUSINESS_SHORTCODE,
         "PhoneNumber": phone_number,
-        "CallBackURL": CALLBACK_URL,
-        "AccountReference": ACCOUNT_REFERENCE,
-        "TransactionDesc": TRANSACTION_DESC,
+        "CallBackURL": config.CALLBACK_URL,
+        "AccountReference": config.ACCOUNT_REFERENCE,
+        "TransactionDesc": config.TRANSACTION_DESC,
     }
     headers = {"Authorization": f"Bearer {_generate_access_token()}"}
     reponse = requests.post(url=stk_push_url, headers=headers, json=payload, timeout=30)
@@ -63,10 +61,61 @@ def query_stk_push(checkout_request_id: str) -> Response:
         "Authorization": f"Bearer {_generate_access_token()}",
     }
     payload = {
-        "BusinessShortCode": BUSINESS_SHORTCODE,
+        "BusinessShortCode": config.BUSINESS_SHORTCODE,
         "Password": _generate_password(),
         "Timestamp": datetime.now().strftime("%Y%m%d%H%M%S"),
         "CheckoutRequestID": checkout_request_id,
     }
     response = requests.post(url=url, headers=headers, json=payload, timeout=30)
+    return response
+
+
+def transaction_status(transaction_id: str) -> Response:
+    """
+    Check the status of a transanction
+
+    :param transaction_id: Unique identifier to identify a transaction on Mpesa
+    """
+    url = "https://sandbox.safaricom.co.ke/mpesa/transactionstatus/v1/query"
+    payload = {
+        "Initiator": config.INITIATOR,
+        "SecurityCredential": config.SECURITY_CREDENTIAL,
+        "CommandID": "TransactionStatusQuery",
+        "TransactionID": transaction_id,
+        "PartyA": config.PARTY_A,
+        "IdentifierType": config.IDENTIFIER_TYPE,
+        "ResultURL": config.RESULT_URL,
+        "QueueTimeOutURL": config.QUEUE_TIMEOUT_URL,
+        "Remarks": config.REMARKS,
+        "Occasion": config.OCCASSION,
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {_generate_access_token()}",
+    }
+    response = requests.post(url=url, json=payload, headers=headers, timeout=30)
+    return response
+
+
+def account_balance() -> Response:
+    """
+    Account Balance API is used to request the account balance of a short code
+    """
+    url = "https://sandbox.safaricom.co.ke/mpesa/accountbalance/v1/query"
+    payload = {
+        "Initiator": config.INITIATOR,
+        "SecurityCredential": config.SECURITY_CREDENTIAL,
+        "CommandID": "AccountBalance",
+        "PartyA": config.PARTY_A,
+        "IdentifierType": config.IDENTIFIER_TYPE,
+        "ResultURL": config.RESULT_URL,
+        "QueueTimeOutURL": config.QUEUE_TIMEOUT_URL,
+        "Remarks": config.REMARKS,
+        "Occasion": config.OCCASSION,
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {_generate_access_token()}",
+    }
+    response = requests.post(url=url, json=payload, headers=headers, timeout=30)
     return response
